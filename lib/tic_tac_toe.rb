33c8917a6f7afd185862059c2648ad590894c195
Amy Westcott <amy.westcott@yahoo.com>
2019-07-12 04:17:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,14 +94,8 @@ class TicTacToe
     end
     
     def play
-        while over? == false
-          turn
-        end
-        if won?
-          puts "Congratulations #{winner}!"
-        elsif draw?
-          puts "Cat's Game!"
-        end
+        turn 
+        if draw?
       end
 
 end
